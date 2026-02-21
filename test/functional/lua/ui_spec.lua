81ad76d8d92cf154984b97095061fb4bf0b8cffd
Wayne Young <wjyoung65@gmail.com>
2026-02-20 19:55:38 -0500

    test(ui): verify the suggestion

--- test/functional/lua/ui_spec.lua
+++ test/functional/lua/ui_spec.lua
@@ -145,7 +145,7 @@ describe('vim.ui', function()
       if not is_os('bsd') then
         local rv = exec_lua([[
           local orig = vim.ui._get_open_cmd
-          if not orig() then
+          if not vim.ui._get_open_cmd() then
             vim.ui._get_open_cmd = function() return {'fake-xdg-open'}, nil end
           end
           local cmd = vim.ui.open('non-existent-file')
